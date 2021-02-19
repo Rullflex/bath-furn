14cdf816b828eace10bfc8392a70da6dc3d02370
Dmitry <libafer99@gmail.com>
2021-02-19 13:04:42 +0500

    second push

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,4 +11,43 @@ document.addEventListener(`DOMContentLoaded`, function () {
             UIkit.modal('#menu').hide()
         })
     })
+
+    const slidegal = (section) => {
+        document.querySelectorAll(`.s${section} .slidegal-item`).forEach((el, key, parent) => {
+            el.addEventListener(`click`, (event) => {
+                const target = event.currentTarget
+                
+                if (!target.classList.contains(`active`)) {
+                    parent.forEach((el, index) => {
+                        if (el === target) {
+                            UIkit.slider(`.s${section}__slider`).show(index)
+                        }
+                    })
+                }
+            })
+        })
+        document.querySelectorAll(`.s${section}__slider .uk-slider-items > li`).forEach((el, idx) => {
+            
+            el.addEventListener(`beforeitemshow`, (event) => {
+                const target = event.target
+                const prev = event.target.previousSibling
+    
+                const showTabNav = (index) => {
+                    document.querySelectorAll(`.s${section} .slidegal-item`).forEach((el, idx) => {
+                        if (idx === index) {
+                            el.classList.add(`active`)
+                        } else {
+                            el.classList.remove(`active`)
+                        }
+                    })
+                }
+    
+                showTabNav([...target.parentElement.children].indexOf(target))
+    
+            })
+        })
+    }
+    
+    slidegal(2)
+    slidegal(4)
 })
